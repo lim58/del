f9a52cfb755ac0645f4dae776b042eebcd01755b
limdayoung <dylim7715@gmail.com>
2023-05-15 19:41:18 +0900

    create : if_greeting.js

---
+++ if_greeting.js
@@ -0,0 +1,12 @@
+let time = 10;
+let greeting;
+
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = " good evening";
+}
+
+console.log(greeting);
